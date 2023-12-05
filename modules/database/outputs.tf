be2a4e480e52c7ffe51ac97fb9a03bcde392bbf1
Le33sia <117647628+Le33sia@users.noreply.github.com>
2023-12-05 18:54:27 +0200

    Update outputs.tf

--- modules/database/outputs.tf
+++ modules/database/outputs.tf
@@ -3,7 +3,6 @@ output "rds_endpoints" {
     for key, instance in aws_db_instance.db_instances :
     key => instance.endpoint
   }
-<<<<<<< HEAD
 }
 
 output "secrets_manager_secret_arn" {
@@ -13,11 +12,3 @@ output "secrets_manager_secret_arn" {
 
 
 
-=======
-}
-
-output "secrets_manager_secret_arn" {
-  value     = aws_secretsmanager_secret.secretdb.arn
-  sensitive = true
-}
->>>>>>> 5a414d40ba8b957aa6985f554151a403a9647e3f
