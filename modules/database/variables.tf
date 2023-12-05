5a414d40ba8b957aa6985f554151a403a9647e3f
Le33sia <117647628+Le33sia@users.noreply.github.com>
2023-12-05 14:55:14 +0200

    Update variables.tf

--- modules/database/variables.tf
+++ modules/database/variables.tf
@@ -1,57 +1,26 @@
 variable "vpc_id" {}
-variable "private_subnet1_id" {}
-variable "private_subnet2_id" {}
+variable "private_subnets" {}
+variable "private_snet_1" {}
+variable "private_snet_2" {}
 
-variable "database_username" {
-  type = string
-}
-variable "database_dbname" {
-  type = string
-}
 variable "secrets_manager_secret_arn" {}
 variable "security_groups" {}
 
-variable "allocated_storage" {
-  description = "Allocated storage"
-  type        = number
-}
 
-variable "engine" {
-  description = "Database engine"
-  type        = string
-}
+variable "db_instances" {
+  description = "Map of configurations for RDS instances"
 
-variable "engine_version" {
-  description = "Database engine version"
-  type        = string
-}
-
-variable "instance_class" {
-  description = "Instance class"
-  type        = string
-}
-
-variable "identifier" {
-  description = "Database identifier"
-  type        = string
-}
-
-variable "parameter_group_name" {
-  description = "Parameter group name"
-  type        = string
-}
-
-variable "multi_az" {
-  description = "Multi-AZ deployment"
-  type        = bool
-}
-
-variable "storage_type" {
-  description = "Storage type"
-  type        = string
-}
-
-variable "skip_final_snapshot" {
-  description = "Skip final snapshot"
-  type        = bool
+  type = map(object({
+    allocated_storage    = number
+    engine               = string
+    engine_version       = string
+    instance_class       = string
+    identifier           = string
+    parameter_group_name = string
+    multi_az             = bool
+    storage_type         = string
+    skip_final_snapshot  = bool
+    db_name              = string
+    username             = string
+  }))
 }
