56f2017bfa68d38326eb1c03ec231e5eb7dd11ed
Le33sia <117647628+Le33sia@users.noreply.github.com>
2023-12-05 19:16:45 +0200

    Update outputs.tf

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 output "vpc_id" {
   value = aws_vpc.demovpc.id
 }
@@ -9,20 +8,3 @@ output "public_subnets" {
 output "private_subnets" {
   value = { for k, v in aws_subnet.private_subnets : k => v.id }
 }
-=======
-output "public_subnet2_id" {
-  value = aws_subnet.public_subnet2.id
-}
-output "public_subnet1_id" {
-  value = aws_subnet.public_subnet1.id
-}
-output "vpc_id" {
-  value = aws_vpc.demovpc.id
-}
-output "private_subnet1_id" {
-  value = aws_subnet.private_subnet1.id
-}
-output "private_subnet2_id" {
-  value = aws_subnet.private_subnet2.id
-}
->>>>>>> 5a414d40ba8b957aa6985f554151a403a9647e3f
